074573b9366e2882af5b95a146a5675c3a47a508
Legolas <The-Legolas>
2026-05-02 00:23:35 +0200

    added some server side and client side loggin

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -22,27 +21,76 @@ func main() {
 	defer conn.Close()
 	fmt.Println("Conneceted to Peril server...")
 
-	newConn, err := conn.Channel()
+	publishCh, err := conn.Channel()
 	if err != nil {
 		log.Fatalf("could not create channel: %v", err)
 	}
 
+	_, _, err = pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilTopic,
+		routing.GameLogSlug,
+		routing.GameLogSlug, // binding key
+		pubsub.SimpleQueueDurable,
+	)
+	if err != nil {
+		log.Fatalf("could not declare and bind queue: %v", err)
+	}
+
 	err = pubsub.PublishJSON(
-		newConn,
-		routing.ExchangePerilDirect,
-		routing.PauseKey,
+		publishCh,
+		routing.ExchangePerilTopic,
+		routing.GameLogSlug,
 		routing.PlayingState{
 			IsPaused: true,
 		},
 	)
 	if err != nil {
-		log.Fatalf("could not publish json: %v", err)
+		log.Fatalf("could not publish time: %v", err)
 	}
 
-	// wait for ctrl+c
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
+	for {
+		gamelogic.PrintServerHelp()
+		inputwords := gamelogic.GetInput()
 
-	fmt.Println("\nShutting Peril server down...")
+		if len(inputwords) == 0 {
+			continue
+		}
+
+		switch inputwords[0] {
+		case "pause":
+			fmt.Println("Publishing paused game state")
+			err = pubsub.PublishJSON(
+				publishCh,
+				routing.ExchangePerilTopic,
+				routing.GameLogSlug,
+				routing.PlayingState{
+					IsPaused: true,
+				},
+			)
+			if err != nil {
+				log.Printf("could not publish time: %v", err)
+			}
+
+		case "resume":
+			fmt.Println("Publishing resumes game state")
+			err = pubsub.PublishJSON(
+				publishCh,
+				routing.ExchangePerilTopic,
+				routing.GameLogSlug,
+				routing.PlayingState{
+					IsPaused: false,
+				},
+			)
+			if err != nil {
+				log.Printf("could not publish time: %v", err)
+			}
+		case "quit":
+			fmt.Println("Publishing quitting game state")
+			return
+		default:
+			fmt.Println("unknown command")
+		}
+	}
+
 }
