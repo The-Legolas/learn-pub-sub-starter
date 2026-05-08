4d26842595362ed4cb05900eadf345cbc7e2d677
Legolas <The-Legolas>
2026-05-08 02:36:43 +0200

    Add: Limit the max queue for consume channels

--- internal/pubsub/consume.go
+++ internal/pubsub/consume.go
@@ -120,6 +120,11 @@ func subscribe[T any](
 		return fmt.Errorf("could not declare and bind queue: %v\n", err)
 	}
 
+	err = ch.Qos(10, 0, false)
+	if err != nil {
+		return fmt.Errorf("could not set QoS: %v\n", err)
+	}
+
 	deliveryChannels, err := ch.Consume(
 		queue.Name, // queue
 		"",         // consumer
